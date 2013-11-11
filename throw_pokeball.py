316e881064be452dc3d26de02531ce6e61b29ad9
Karen Clark <kiroons@gmail.com>
2013-11-10 19:44:58 -0600

    Fixed bugs.

--- throw_pokeball.py
+++ throw_pokeball.py
@@ -3,29 +3,34 @@
 import random
 import sqlite3
 
-conn = sqlite3.connect('Pokemon.db')
-
-name = input("Enter your name: ")
-pokemon = input("Enter pokemon: ")
-
-print("%s threw a pokeball at %s!" % (name, pokemon))
-
-if (random.random() <= 0.25):
-    print('You caught %s!' % (pokemon))
-    nick = input('Enter a nickname: ')
-    insert_into_db(pokemon, nick)
-    print('Pokemon you\'ve caught:\n')
-    list_caught()
-    
-else:
-    print('%s got away..' % (pokemon))
-
 def insert_into_db(pokemon, nick):
     c = conn.cursor()
-    c.execute('SELECT id FROM Pokemon WHERE name = %s', (pokemon))
-    poke_id = c.fetchone()
-    c.execute('INSERT INTO Caught VALUES (%s, %s)', (pokemon, nick))
+    c.execute('SELECT id FROM Pokemon WHERE name = ?', (pokemon, ))
+    poke_id = c.fetchone()[0]
+    c.execute('INSERT INTO Caught VALUES (?, ?)', (poke_id, nick))
+    conn.commit()
 
 def list_caught():
-    for row in c.execute('SELECT * FROM Caught JOIN Pokemon ON Caught.id = Pokemon.id ORDER BY Pokemon.name'):
+    c = conn.cursor()
+    for row in c.execute('SELECT name, type, nickname FROM Caught JOIN Pokemon ON Caught.id = Pokemon.id ORDER BY Pokemon.name'):
         print(row)
+
+conn = sqlite3.connect('Pokemon.db')
+
+if __name__ == "__main__":
+
+    name = input("Enter your name: ")
+    pokemon = input("Enter pokemon: ")
+    
+    print("%s threw a pokeball at %s!" % (name, pokemon))
+
+    if (random.random() <= 0.25):
+        print('You caught %s!' % (pokemon))
+        nick = input('Enter a nickname: ')
+        insert_into_db(pokemon, nick)
+        print("Pokemon you've caught:")
+        list_caught()
+    else:
+        print('%s got away..' % (pokemon))
+
+    conn.close()
